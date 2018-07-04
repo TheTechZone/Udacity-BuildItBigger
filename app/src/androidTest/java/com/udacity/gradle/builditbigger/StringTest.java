871f754d0b02d86b42ae69d4c56993eaba2ae279
Adrian <dielorg@gmail.com>
2018-07-04 10:12:44 +0300

    added gradle task

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/StringTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/StringTest.java
@@ -1,10 +1,37 @@
 package com.udacity.gradle.builditbigger;
 
-import android.test.AndroidTestCase;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.InstrumentationTestCase;
+import android.util.Log;
 
-public class StringTest extends AndroidTestCase {
-    public void test(){
-        JokeEndpointTask task = new JokeEndpointTask();
-        task.execute();
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+
+@RunWith(AndroidJUnit4.class)
+public class StringTest extends InstrumentationTestCase {
+
+    Context context;
+
+    @Test
+    public void test() throws InterruptedException {
+        assertTrue(true);
+        final CountDownLatch latch = new CountDownLatch(1);
+        context = InstrumentationRegistry.getContext();
+        AsyncTask<Context, Void, String> testTask = new JokeEndpointTask() {
+            @Override
+            protected void onPostExecute(String result) {
+                Log.e("TEST", result);
+                assertNotNull(result);
+                assertTrue(result.length() > 0);
+                latch.countDown();
+            }
+        };
+        testTask.execute(context);
+        latch.await();
     }
 }
